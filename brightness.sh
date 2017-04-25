01dff47e387d3ca52896277f85767539661dc251
flgc <abotes@gmail.com>
2017-04-25 13:59:53 +0900

    feat: Connected monitor auto-detected

--- brightness.sh
+++ brightness.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
 # Increase or decrease brightness
+function fnc_help {
+    echo "Depends on 'xrandr'."
+    echo ""
+    echo "To set brightness:"
+    echo "$ ./brightness.sh u n     # = brightness 'up' by n"
+    echo "$ ./brightness.sh d n     # = brightness 'down' by n"
+    echo "n is a value between 0.01 and 0.2."
+    echo ""
+    echo "To show brightness:"
+    echo "$ ./brightness.sh s       # = 'show' brightness for active monitor"
+    echo ""
+}
 
 direction=$1
 amount=$2
-brightness=$(xrandr --verbose | awk '/Brightness/ { print $2; exit }')
 
 if ! [[ -n "$direction" ]]; then
-    exit 1
+  fnc_help
+  exit 1
 fi
 
+# brightness=$(xrandr --verbose | awk '/Brightness/ { print $2; exit }')
+
+# attempt to get active screen and brightness value
+# haven't tested this on multiple screens. if it works at all, it will only dim one screen..
+# access array items with: ${screen[n]}
+screen=($(xrandr --verbose | awk '/ connected/{screen=$1}/Brightness/{brightness=$2}END{print (screen, brightness)}'))
+echo "Connected monitor detected:" ${screen[0]}
+
 if [[ "$direction" = 's' ]]; then
-  echo "Current brightness is:" $brightness
+  echo "Brightness:" ${screen[1]}
   exit 0
 fi
 
-if [[ -n "$amount" ]] && [[ -n "$brightness" ]]; then
+if [[ -n "$amount" ]] && [[ -n "$screen" ]]; then
 
   if (( $(echo "$amount > 0.2" | bc -l) )); then
     amount=0.2
@@ -26,32 +46,28 @@ if [[ -n "$amount" ]] && [[ -n "$brightness" ]]; then
   fi
 
   if [[ "$direction" = 'u' ]]; then
-    echo 'increase brightness'
-    newbrightness=$(bc <<< $brightness+$amount)
+    echo 'Increase brightness'
+    newbrightness=$(bc <<< ${screen[1]}+$amount)
     if (( $(echo "$newbrightness > 1.0" | bc -l) )); then
+      echo "Brightness cannot exceed 1."
       newbrightness=1
     fi
 
   elif [[ "$direction" = 'd' ]]; then
-    echo 'decrease brightness'
-    newbrightness=$(bc <<< $brightness-$amount)
+    echo 'Decrease brightness'
+    newbrightness=$(bc <<< ${screen[1]}-$amount)
     if (( $(echo "$newbrightness < 0.0" | bc -l) )); then
+      echo "Brightness cannot be lower than 0."
       newbrightness=0
     fi
    fi
 
-  xrandr --output LVDS-1 --brightness $newbrightness
+  xrandr --output ${screen[0]} --brightness $newbrightness
   echo "Brightness after: " $newbrightness
 
 else
-    echo "Depends on 'xrandr'."
-    echo ""
-    echo "To set brightness:"
-    echo "$ ./brightness.sh u n     # = brightness 'up' by n"
-    echo "$ ./brightness.sh d n     # = brightness 'down' by n"
-    echo "n is a value between 0.01 and 0.2."
-    echo ""
-    echo "$ ./brightness.sh s       # = 'show' brightness for active monitor"
+  fnc_help
+  exit 1
 fi
 
 
